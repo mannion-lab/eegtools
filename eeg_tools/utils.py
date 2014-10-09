9fb9fbd2132eeafe6d190a7fda366aefb88fc33e
damien <damien@localhost>
2014-10-09 15:21:16 +1100

    -

--- eeg_tools/utils.py
+++ eeg_tools/utils.py
@@ -2,6 +2,109 @@
 
 import os
 import datetime
+import tempfile
+
+import mne
+
+import runcmd
+
+
+def convert_bdf_to_fiff(bdf_path, fif_path, pos_path=None, overwrite=True):
+    """Convert a dataset in BioSemi BDF format into MNE FIF format, adding
+    digitiser information and fixing lost information along the way.
+
+    Parameters
+    ----------
+    bdf_path, fif_path: string
+        Paths to the input and output, respectively.
+    pos_path: string, optional
+        Path to a Polhemus localiser file. This gets converted to a HPTS file.
+    overwrite: bool, optional
+        Whether its ok to overwrite `fif_path`
+
+    """
+
+    if pos_path is not None:
+        # first step is to convert the POS file to HPTS
+        (pos_base, pos_ext) = os.path.splitext(pos_path)
+
+        hpts_path = os.path.join(pos_base, ".hpts")
+
+        convert_fastrak_to_hpts(
+            pos_path=pos_path,
+            hpts_path=hpts_path,
+            overwrite=overwrite
+        )
+    else:
+        hpts_path = None
+
+    raw = mne.io.read_raw_edf(
+        input_fname=bdf_path,
+        hpts=hpts_path,
+        verbose="warning"
+    )
+
+
+
+def get_unsw_channel_rename_mapping():
+
+
+
+
+def fix_channel_types(fif_path, alias_path=None):
+    """Assign channels in a FIF file to the correct type, which is lost when
+    converting from BDF format.
+
+    Parameters
+    ----------
+    fif_path: string
+        Path to the FIF file to alter.
+    alias_path: string, optional
+        Path to the channel change information file. If not given, a standard
+        based on the 64-channel fix at UNSW is applied.
+
+    """
+
+    if alias_path is None:
+
+        eog_chans = ["EXG" + str(chan) for chan in [1, 2, 5]]
+        misc_chans = ["EXG" + str(chan) for chan in [3, 4, 6, 7, 8]]
+
+        misc_chans.extend(["Erg" + str(chan) for chan in [1, 2]])
+        misc_chans.extend(["GSR" + str(chan) for chan in [1, 2]])
+        misc_chans.extend(["Resp", "Plet", "Temp"])
+
+        eog_code = 202
+        misc_code = 502
+
+        chan_out = [
+            ":".join([chan_name] * 2 + [str(eog_code)])
+            for chan_name in eog_chans
+        ]
+
+        chan_out.extend(
+            [
+                ":".join([chan_name] * 2 + [str(misc_code)])
+                for chan_name in misc_chans
+            ]
+        )
+
+        alias_path = tempfile.NamedTemporaryFile(delete=False)
+
+        alias_path.file.write("\n".join(chan_out))
+
+        alias_path.close()
+
+        alias_path = alias_path.name
+
+    cmd = [
+        "mne_rename_channels",
+        "--fif", fif_path,
+        "--alias", alias_path
+    ]
+
+    runcmd.run_cmd(" ".join(cmd))
+
 
 
 def convert_fastrak_to_hpts(pos_path, hpts_path, overwrite=False):
@@ -18,11 +121,6 @@ def convert_fastrak_to_hpts(pos_path, hpts_path, overwrite=False):
     overwrite: bool, optional
         Whether to overwrite ``hpts_path``, if it exists.
 
-    Notes
-    -----
-    * The first electrode that is digitised is assigned as the reference
-      electrode (label of 0)
-
     """
 
     if overwrite and os.path.exists(hpts_path):
@@ -36,8 +134,8 @@ def convert_fastrak_to_hpts(pos_path, hpts_path, overwrite=False):
     with open(pos_path, "r") as pos_file:
         raw_data = pos_file.readlines()
 
-    i_eeg = 0
-    i_extra = 0
+    i_eeg = 1
+    i_extra = 1
 
     # ignore the first
     for pos_data in raw_data[1:]:
